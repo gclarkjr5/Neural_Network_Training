1814bddf90d6bfa632603e71da0a5af5888a0684
gclarkjr5 <gclarkjr5@gmail.com>
2018-06-13 14:02:31 -0500

    Add comments for clarity

--- Basic_NN.R
+++ Basic_NN.R
@@ -1,14 +1,19 @@
+# Clear environment
 rm(list = ls())
 
+# Load libraries
 library(ggplot2)
 library(dplyr)
 
-# input neurons
+# input data to input neurons
+# 4x3 matrix
 training_input = matrix(
   c(0,0,1,1,1,1,1,0,1,0,1,1),
   nrow = 4,
   ncol = 3)
 
+# Output data
+# 1x4 matrix (transpose it to make it 4x1)
 training_ouput = t(matrix(
   c(0,1,1,0),
   nrow = 1,
@@ -18,6 +23,7 @@ training_ouput = t(matrix(
 # create rarndom weights
 set.seed(0415)
 weights = round(runif(3, min = -1, max = 1), 2)
+# turn array into 3x1 matrix
 weightsMat = matrix(
   weights,
   nrow = 3,
@@ -25,15 +31,28 @@ weightsMat = matrix(
 )
 
 
-# iterate
+# create empty vectors for visualization purposes
 outputVec = c()
 weightsVec = c()
 index = c()
+
+# iterate
 for(i in 1:10000) {
-  index[i] = i
+  index[i] = i # add to index vector
+  
+  # activation function (sigmoid), X is the dot product between input and weights
   output = 1/(1 + exp(-training_input %*% weightsMat))
-  outputVec[i] = output
-  weightsMat = weightsMat + (t(training_input) %*% ((training_ouput - output) * output * (1- output)))
+  outputVec[i] = output # add to output vector
+  
+  # calculate the error
+  error = training_ouput - output
+  
+  # calculate the gradient (derivative of the activation function)
+  sigmoidDeriv = output * (1 - output)
+  
+  # adjustment is dot T(input) dot (error * gradient)
+  adjustment = t(training_input) %*% (error * sigmoidDeriv)
+  weightsMat = weightsMat + adjustment
   weightsVec[i] = weightsMat
 }
 
